0ff57d4cbd06a602bf1d37c9dafc3f19a2811232
KEITUMETSE PRECIOUS LEFASO <precious.lefaso@gmail.com>
2025-10-31 21:13:40 +0200

    Add ETL job to process and write data to S3

--- glue_scripts/etl_job.py
+++ glue_scripts/etl_job.py
@@ -1,1 +1,22 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from awsglue.context import GlueContext
+from pyspark.context import SparkContext
+from awsglue.job import Job
 
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+datasource = glueContext.create_dynamic_frame.from_catalog(database="default", table_name="raw_data")
+transformed = datasource.drop_fields(['unnecessary_column'])
+glueContext.write_dynamic_frame.from_options(
+    frame=transformed, connection_type="s3",
+    connection_options={"path": "s3://processed-bucket/"},
+    format="json"
+)
+job.commit()
